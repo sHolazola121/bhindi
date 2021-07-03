3e86f4de1ba6f70718cdd12bd76a33eb183bd28a
sHolazola121 <86874245+sHolazola121@users.noreply.github.com>
2021-07-03 23:13:53 +0530

    Update bhindi.sh

--- bhindi.sh
+++ bhindi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TFQCDdbTa4C2YUi9aourz4ekKKnPnaxEfF
+WALLET=WIN:TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#at9p-rbx8)
 chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
